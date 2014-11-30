28358ff87a5f86719d56da49cbf124f845f81a4f
Atmelfan <gustavp@gpa-robotics.om>
2014-12-01 00:11:45 +0100

    Comments

--- asm/test.s
+++ asm/test.s
@@ -1,11 +1,18 @@
 #Author: atmelfan
 $include arch.s
 
+#init
 init:
-	ldi 1 R0
+	#Kopiera 1 till register 0
+	micke 1 R0
+	#Addera 255 till register 0
 	adi 255 R0
-	#sxt 1 R0
+#main
+
 main:
+	#Addera 1 till register 0
 	adi 1 R0
+	#Hoppa
 	jmp R0 R0
+	#till main
 	+main
